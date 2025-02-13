44c841f3a91c992eb6c8f28e13ddef613fca09b6
Chet Murthy <chetsky@gmail.com>
2025-02-12 19:03:55 -0800

    meh

--- tests/pa_ppx_fmtformat_test.ml
+++ tests/pa_ppx_fmtformat_test.ml
@@ -7,10 +7,12 @@ let test_simple ctxt =
   ()
   ; assert_equal "" [%fmt_str ""]
   ; (let c = "argle" in assert_equal ~printer "a b c argle d e f" {%fmt_str|a b c $(c) d e f|})
-  ; (let c = "argle" in assert_equal ~printer "a b c argle d e f" {%fmt_str|a b c $("argle") d e f|})
-  ; (let c = "argle" in assert_equal ~printer "a b c argle d e f" {%fmt_str|a b c $("argle"|string) d e f|})
-  ; (let c = "argle" in assert_equal ~printer {|a b c "argle" d e f|} {%fmt_str|a b c $("argle"|Dump.string) d e f|})
-
+  ; (assert_equal ~printer "a b c argle d e f" {%fmt_str|a b c $("argle") d e f|})
+  ; (assert_equal ~printer "a b c argle d e f" {%fmt_str|a b c $("argle"|string) d e f|})
+  ; (assert_equal ~printer {|a b c "argle" d e f|} {%fmt_str|a b c $("argle"|Dump.string) d e f|})
+  ; (let c = "argle" in assert_equal ~printer "a b c argle d e f" {%fmt_str|a b c $(|c|) d e f|})
+  ; (let c = "argle" in assert_equal ~printer "a b c argle d e f" {%fmt_str|a b c $(|c|string|) d e f|})
+  ; (assert_equal ~printer "a b c argle d e f" {%fmt_str|a b c $(|"argle"|string|) d e f|})
 let suite = "Test pa_ppx_fmtformat" >::: [
       "simple"   >:: test_simple
     ]
